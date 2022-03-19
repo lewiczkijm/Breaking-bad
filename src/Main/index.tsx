ea3c117857f5abfa512191713b30d5bfc0e239cf
mlevitsky <Mikhail.Levitsky@lanit-tercom.com>
2022-03-19 10:17:25 +0200

    feat: filter episodes by season end by page

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,5 +1,8 @@
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {EpisodeItem} from "./EpisodeItem";
+import {generateArray} from "../helpers";
+
+const ITEMS_PER_PAGE = 10
 
 const getEpisodes = async ():Promise<Array<any> | undefined> =>{
     let res = await fetch(`/episodes`).catch(()=>undefined)
@@ -7,20 +10,28 @@ const getEpisodes = async ():Promise<Array<any> | undefined> =>{
     return await res.json()
 }
 
-
 export const Main = ()=>{
-    const [episodes,setEpisodes] = useState<Array<any>>([])
+    const [episodes,setEpisodes] = useState<Array<any>>(()=>generateArray({},10))
+    const [page, setPage] = useState(1)
+    const [season, setSeason] = useState<string | undefined>(undefined)
+
     const getAllEpisodes = async ()=>{
         const episodes = await getEpisodes();
         if(!episodes) return // error processing
         setEpisodes(episodes)
     }
+
     useEffect(()=>{
         getAllEpisodes()
     },[])
+
+    const presentEpisodes = useMemo(
+        ()=>episodes.filter(episode=>season?episode.season === season:true)
+            .slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE)
+        ,[episodes,page,season])
+
     return <div style={{marginLeft:10,marginRight:10}}>
         <div className={"block"}></div>
-        <EpisodeItem />
-        {episodes.map(episode=><EpisodeItem id={episode.episode_id} title={episode.title} airDate={episode.air_date} key={episode.episode_id}/>)}
+        {presentEpisodes.map(episode=><EpisodeItem id={episode.episode_id} title={episode.title} airDate={episode.air_date} key={episode.episode_id}/>)}
     </div>
 }
--- src/helpers.tsx
+++ src/helpers.tsx
@@ -9,3 +9,12 @@ export const Skeleton = ({children,style}:skeletonType)=><span
     style={style}
     className={"skeleton"}
 >{children}</span>
+
+// Generation array with default element value and declared size. Object value will link copy
+export function generateArray<T>(val:T, count:number):T[]{
+    let arr = []
+    for(let i = 0; i < count;i ++){
+        arr.push(val)
+    }
+    return arr
+}
